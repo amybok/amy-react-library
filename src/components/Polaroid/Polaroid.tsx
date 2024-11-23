e2c45c17c687000bcc1c682b358c7a86607c6b72
Amy <amy.gh.ly@gmail.com>
2024-11-24 01:15:43 +1100

    fixed style

--- src/components/Polaroid/Polaroid.tsx
+++ src/components/Polaroid/Polaroid.tsx
@@ -2,10 +2,10 @@ import React from "react";
 import "./Polaroid.css";
 
 interface PolaroidProps {
-    
+    title?: string;
 }
 
-const Polaroid  = (props: PolaroidProps) => {
+const Polaroid : React.FC<PolaroidProps>  = ({title}) => {
     return (
         <div className="background">
             <div className="image">
@@ -13,7 +13,7 @@ const Polaroid  = (props: PolaroidProps) => {
             </div>
             <div className="overlay tag"/>
             <div className="text"> 
-
+                {title} Symphony
             </div>
 
         </div>
