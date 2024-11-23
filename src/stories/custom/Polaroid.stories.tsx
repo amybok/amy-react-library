56ca4843fcca51f13ae6680b9e29e7a5202ed4b8
Amy <amy.gh.ly@gmail.com>
2024-11-24 01:29:53 +1100

    reorganised stories folder

--- src/stories/custom/Polaroid.stories.tsx
+++ src/stories/custom/Polaroid.stories.tsx
@@ -1,5 +1,5 @@
 import { StoryObj, Meta } from "@storybook/react";
-import Polaroid from "../components/Polaroid/Polaroid";
+import Polaroid from "../../components/Polaroid";
 
 export default {
     title: "ReactComponentLibrary/Polaroid",
